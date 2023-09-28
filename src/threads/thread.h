7b022ec3de3958943d55573ddff48fc304b79c84
unajun <bjyoon513@gmail.com>
2023-09-28 22:26:55 +0900

    현시점에 필요 없는 프로토 타입 삭제

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -117,8 +117,6 @@ tid_t thread_create(const char *name, int priority, thread_func *, void *);
 void thread_block(void);
 void thread_unblock(struct thread *);
 
-void donate_priority(void);
-
 struct thread *thread_current(void);
 tid_t thread_tid(void);
 const char *thread_name(void);
