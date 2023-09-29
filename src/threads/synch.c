bf6f6fd2bda3b4021dd5dbd464732e1d2f39e73d
unajun <bjyoon513@gmail.com>
2023-09-30 02:29:32 +0900

    intr off 상태로 sema_up 할때, preemption 체크 하도록 변경

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -99,28 +99,24 @@ bool sema_try_down(struct semaphore *sema) {
 
    This function may be called from an interrupt handler. */
 void sema_up(struct semaphore *sema) {
-  enum intr_level old_level;
-  bool check_preempt = false;
-
   ASSERT(sema != NULL);
+
+  enum intr_level old_level;
+  bool yield_on_return = false;
 
   old_level = intr_disable();
   if (!list_empty(&sema->waiters)) {
-    check_preempt = true;
     list_sort(&sema->waiters, compare_thread_priority, NULL);
     thread_unblock(list_entry(list_pop_front(&sema->waiters),
                               struct thread, elem));
-  }
-  sema->value++;
-  if (check_preempt && is_preemptive()) {
-    if (intr_context()) {
-      intr_yield_on_return();
-    } else {
-      thread_yield();
+    if(is_preemptive()){
+      yield_on_return = true;
     }
   }
-
+  sema->value++;
   intr_set_level(old_level);
+  if (!intr_context() && yield_on_return)
+    thread_yield();
 }
 
 static void sema_test_helper(void *sema_);
