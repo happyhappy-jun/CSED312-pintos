829df4f1dc30f07de394876d2038746a443255c3
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-09-29 21:40:30 +0900

    compare_thread_wakeup_tick() 구현

--- src/threads/compare.c
+++ src/threads/compare.c
@@ -12,3 +12,13 @@ bool compare_thread_priority(const struct list_elem *a, const struct list_elem *
   struct thread, elem) -> priority;
   return priority_a > priority_b;
 }
+
+bool compare_thread_wakeup_tick(const struct list_elem *a,
+                                const struct list_elem *b void *aux) {
+  const struct sleep_list_elem *elem_a =
+      list_entry(a, struct sleep_list_elem, elem);
+  const struct sleep_list_elem *elem_b =
+      list_entry(b, struct sleep_list_elem, elem);
+
+  return elem_a->end_tick < elem_b->end_tick;
+}
