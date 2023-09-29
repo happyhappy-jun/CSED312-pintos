7a4e3cd02f631d1e9b0ad7694b0c93b7eae71f5c
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-09-29 21:40:30 +0900

    compare_thread_wakeup_tick() 구현

--- src/threads/compare.c
+++ src/threads/compare.c
@@ -3,7 +3,6 @@
 //
 
 #include "compare.h"
-#include "threads/synch.h"
 #include "threads/thread.h"
 
 bool compare_thread_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
@@ -16,12 +15,12 @@ bool compare_thread_priority(const struct list_elem *a, const struct list_elem *
   return priority_a > priority_b;
 }
 
-bool compare_sema_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
-  struct semaphore_elem *sema_a = list_entry(a, struct semaphore_elem, elem);
-  struct semaphore_elem *sema_b = list_entry(b, struct semaphore_elem, elem);
+bool compare_thread_wakeup_tick(const struct list_elem *a,
+                                const struct list_elem *b void *aux) {
+  const struct sleep_list_elem *elem_a =
+      list_entry(a, struct sleep_list_elem, elem);
+  const struct sleep_list_elem *elem_b =
+      list_entry(b, struct sleep_list_elem, elem);
 
-  struct thread *thread_a = list_entry(list_begin(&sema_a->semaphore.waiters), struct thread, elem);
-  struct thread *thread_b = list_entry(list_begin(&sema_b->semaphore.waiters), struct thread, elem);
-
-  return thread_a->priority > thread_b->priority;
+  return elem_a->end_tick < elem_b->end_tick;
 }
