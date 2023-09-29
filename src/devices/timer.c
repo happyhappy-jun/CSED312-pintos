39361bca0ac6a5102f44dacf938b5314cd4a49c2
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-09-29 22:11:13 +0900

    timer.c에서 thread_sleep(), thread_wakeup() 사용

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -88,8 +88,7 @@ timer_sleep(int64_t ticks) {
   int64_t start = timer_ticks();
 
   ASSERT(intr_get_level() == INTR_ON);
-  while (timer_elapsed(start) < ticks)
-    thread_yield();
+  thread_sleep(start + ticks);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -162,6 +161,7 @@ static void
 timer_interrupt(struct intr_frame *args UNUSED) {
   ticks++;
   thread_tick();
+  thread_wakeup(ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
