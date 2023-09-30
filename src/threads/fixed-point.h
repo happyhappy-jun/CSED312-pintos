d28c16d338f7a7c729b89801860cc9d774a4f66d
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-09-30 23:19:01 +0900

    fixed-point 함수에 fp prefix 추가

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -10,13 +10,13 @@ typedef int fixed_t;
 fixed_t int2fp(int n);
 int fp2int(fixed_t x);
 int fp2int_round(fixed_t x);
-fixed_t add_y(fixed_t x, fixed_t y);
-fixed_t add_n(fixed_t x, int n);
-fixed_t sub_y(fixed_t x, fixed_t y);
-fixed_t sub_n(fixed_t x, int n);
-fixed_t mul_y(fixed_t x, fixed_t y);
-fixed_t mul_n(fixed_t x, int n);
-fixed_t div_y(fixed_t x, fixed_t y);
-fixed_t div_n(fixed_t x, int n);
+fixed_t fp_add_y(fixed_t x, fixed_t y);
+fixed_t fp_add_n(fixed_t x, int n);
+fixed_t fp_sub_y(fixed_t x, fixed_t y);
+fixed_t fp_sub_n(fixed_t x, int n);
+fixed_t fp_mul_y(fixed_t x, fixed_t y);
+fixed_t fp_mul_n(fixed_t x, int n);
+fixed_t fp_div_y(fixed_t x, fixed_t y);
+fixed_t fp_div_n(fixed_t x, int n);
 
 #endif//PINTOS_SRC_THREADS_FIXED_H_
