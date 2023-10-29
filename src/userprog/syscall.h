02b6a576f5dfe47c75e3a2e9770491026e2bb88e
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-10-29 18:58:21 +0900

    typo 수정

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -17,6 +17,6 @@ int sys_read(int, void *, unsigned);
 int sys_write(int, void *, unsigned);
 void sys_seek(int, unsigned);
 unsigned sys_tell(int);
-void sys_close(int)
+void sys_close(int);
 
 #endif /* userprog/syscall.h */
