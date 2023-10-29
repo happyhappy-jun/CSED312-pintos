55008a95356adc482a02fae6c8e1bb7765b87394
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-10-29 18:57:29 +0900

    syscall 기본 구현

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,4 +1,5 @@
 #include "userprog/syscall.h"
+#include "devices/shutdown.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include <stdio.h>
@@ -6,12 +7,85 @@
 
 static void syscall_handler(struct intr_frame *);
 
+int get_from_user_stack(const int *, int);
+int get_syscall_n(void *);
+void get_syscall_args(void *, int, int *);
+
+int get_from_user_stack(const int *esp, int offset) {
+  // need check for accessing user memory
+  return *(esp + offset);
+}
+
+int get_syscall_n(void *esp) {
+  return get_from_user_stack(esp, 1);
+}
+
+void get_syscall_args(void *esp, int n, int *syscall_args) {
+  for (int i = 0; i < n; i++)
+    syscall_args[i] = get_from_user_stack(esp, 1 + i);
+}
+
 void syscall_init(void) {
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler(struct intr_frame *f UNUSED) {
-  printf("system call!\n");
-  thread_exit();
+static void syscall_handler(struct intr_frame *f) {
+  int syscall_n;
+  int syscall_arg[3];
+
+  syscall_n = get_syscall_n(f->esp);
+  switch (syscall_n) {
+  case SYS_HALT:
+    shutdown_power_off();
+  case SYS_EXIT:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    sys_exit(syscall_arg[0]);
+    break;
+  case SYS_EXEC:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    f->eax = sys_exec((const char *) syscall_arg[0]);
+    break;
+  case SYS_WAIT:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    f->eax = sys_wait(syscall_arg[0]);
+    break;
+  case SYS_CREATE:
+    get_syscall_args(f->esp, 2, syscall_arg);
+    f->eax = sys_create((const char *) syscall_arg[0], syscall_arg[1]);
+    break;
+  case SYS_REMOVE:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    f->eax = sys_remove((const char *) syscall_arg[0]);
+    break;
+  case SYS_OPEN:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    f->eax = sys_open((const char *) syscall_arg[0]);
+    break;
+  case SYS_FILESIZE:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    f->eax = sys_filesize(syscall_arg[0]);
+    break;
+  case SYS_READ:
+    get_syscall_args(f->esp, 3, syscall_arg);
+    f->eax = sys_read(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
+    break;
+  case SYS_WRITE:
+    get_syscall_args(f->esp, 3, syscall_arg);
+    f->eax = sys_write(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
+    break;
+  case SYS_SEEK:
+    get_syscall_args(f->esp, 2, syscall_arg);
+    sys_seek(syscall_arg[0], syscall_arg[1]);
+    break;
+  case SYS_TELL:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    f->eax = sys_tell(syscall_arg[0]);
+    break;
+  case SYS_CLOSE:
+    get_syscall_args(f->esp, 1, syscall_arg);
+    sys_close(syscall_arg[0]);
+    break;
+  default:
+    break;
+  }
 }
