599b22a61933ad77252ff3916866d004f61c5643
happyhappy-jun <bjyoon513@gmail.com>
2023-11-01 02:37:16 +0900

    Revert "create, remove 구현"
    This reverts commit 719e009a2890fbef2b33a5e30537a56ef39b40e1.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,6 +1,5 @@
 #include "userprog/syscall.h"
 #include "devices/shutdown.h"
-#include "filesys/filesys.h"
 #include "threads/interrupt.h"
 #include "threads/palloc.h"
 #include "threads/thread.h"
@@ -8,7 +7,24 @@
 #include <stdio.h>
 #include <syscall-nr.h>
 
-struct lock file_lock;
+static void syscall_handler(struct intr_frame *);
+
+static void sys_exit(int);
+static pid_t sys_exec(const char *);
+static int sys_wait(pid_t);
+
+static bool sys_create(const char *, unsigned initial_size);
+static bool sys_remove(const char *);
+static int sys_open(const char *);
+static int sys_filesize(int);
+static int sys_read(int, void *, unsigned);
+static int sys_write(int, void *, unsigned);
+static void sys_seek(int, unsigned);
+static unsigned sys_tell(int);
+
+static int get_from_user_stack(const int *, int);
+static int get_syscall_n(void *);
+static void get_syscall_args(void *, int, int *);
 
 static int get_from_user_stack(const int *esp, int offset) {
   int value;
@@ -26,7 +42,6 @@ static void get_syscall_args(void *esp, int n, int *syscall_args) {
 }
 
 void syscall_init(void) {
-  lock_init(&file_lock);
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -34,64 +49,49 @@ static void syscall_handler(struct intr_frame *f) {
   int syscall_n;
   int syscall_arg[3];
 
-  if (!validate_uaddr(f->esp))
-    sys_exit(-1);
-
   syscall_n = get_syscall_n(f->esp);
   switch (syscall_n) {
-  case SYS_HALT: shutdown_power_off();
-  case SYS_EXIT:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    sys_exit(syscall_arg[0]);
-    break;
-  case SYS_EXEC:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    f->eax = sys_exec((const char *) syscall_arg[0]);
-    break;
-  case SYS_WAIT:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    f->eax = sys_wait(syscall_arg[0]);
-    break;
-  case SYS_CREATE:
-    get_syscall_args(f->esp, 2, syscall_arg);
-    f->eax = sys_create((const char *) syscall_arg[0], syscall_arg[1]);
-    break;
-  case SYS_REMOVE:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    f->eax = sys_remove((const char *) syscall_arg[0]);
-    break;
-  case SYS_OPEN:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    // TODO: f->eax = sys_open((const char *) syscall_arg[0]);
-    break;
-  case SYS_FILESIZE:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    // TODO: f->eax = sys_filesize(syscall_arg[0]);
-    break;
-  case SYS_READ:
-    get_syscall_args(f->esp, 3, syscall_arg);
-    // TODO: f->eax = sys_read(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
-    break;
-  case SYS_WRITE:
-    get_syscall_args(f->esp, 3, syscall_arg);
-    f->eax = sys_write(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
-    break;
-  case SYS_SEEK:
-    get_syscall_args(f->esp, 2, syscall_arg);
-    // TODO: sys_seek(syscall_arg[0], syscall_arg[1]);
-    break;
-  case SYS_TELL:
-    get_syscall_args(f->esp, 1, syscall_arg);
-    // TODO: f->eax = sys_tell(syscall_arg[0]);
-    break;
-  case SYS_CLOSE:
-    get_syscall_args(f->esp, 3, syscall_arg);
-    // TODO: f->eax = sys_read(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
-  default: break;
+    case SYS_HALT:shutdown_power_off();
+    case SYS_EXIT:get_syscall_args(f->esp, 1, syscall_arg);
+      sys_exit(syscall_arg[0]);
+      break;
+    case SYS_EXEC:get_syscall_args(f->esp, 1, syscall_arg);
+      f->eax = sys_exec((const char *) syscall_arg[0]);
+      break;
+    case SYS_WAIT:get_syscall_args(f->esp, 1, syscall_arg);
+      f->eax = sys_wait(syscall_arg[0]);
+      break;
+    case SYS_CREATE:get_syscall_args(f->esp, 2, syscall_arg);
+      // f->eax = sys_create((const char *) syscall_arg[0], syscall_arg[1]);
+      break;
+    case SYS_REMOVE:get_syscall_args(f->esp, 1, syscall_arg);
+      // f->eax = sys_remove((const char *) syscall_arg[0]);
+      break;
+    case SYS_OPEN:get_syscall_args(f->esp, 1, syscall_arg);
+      // f->eax = sys_open((const char *) syscall_arg[0]);
+      break;
+    case SYS_FILESIZE:get_syscall_args(f->esp, 1, syscall_arg);
+      // f->eax = sys_filesize(syscall_arg[0]);
+      break;
+    case SYS_READ:get_syscall_args(f->esp, 3, syscall_arg);
+      // f->eax = sys_read(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
+      break;
+    case SYS_WRITE:get_syscall_args(f->esp, 3, syscall_arg);
+      f->eax = sys_write(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
+      break;
+    case SYS_SEEK:get_syscall_args(f->esp, 2, syscall_arg);
+      // sys_seek(syscall_arg[0], syscall_arg[1]);
+      break;
+    case SYS_TELL:get_syscall_args(f->esp, 1, syscall_arg);
+      // f->eax = sys_tell(syscall_arg[0]);
+      break;
+    case SYS_CLOSE:get_syscall_args(f->esp, 3, syscall_arg);
+      // f->eax = sys_read(syscall_arg[0], (void *) syscall_arg[1], syscall_arg[2]);
+    default:break;
   }
 }
 
-void sys_exit(int status) {
+static void sys_exit(int status) {
   struct thread *cur = thread_current();
   cur->pcb->exit_code = status;
   printf("%s: exit(%d)\n", cur->name, status);
@@ -124,28 +124,4 @@ int sys_write(int fd, void *buffer, unsigned int size) {
   }
 
   return -1;
-}
-
-static bool sys_create(const char *file, unsigned initial_size) {
-  if (file == NULL || !validate_uaddr(file)) {
-    sys_exit(-1);
-  }
-
-  lock_acquire(&file_lock);
-  bool success = filesys_create(file, (off_t) initial_size);
-  lock_release(&file_lock);
-
-  return success;
-}
-
-static bool sys_remove(const char *file) {
-  if (file == NULL || !validate_uaddr(file)) {
-    sys_exit(-1);
-  }
-
-  lock_acquire(&file_lock);
-  bool success = filesys_remove(file);
-  lock_release(&file_lock);
-
-  return success;
 }
