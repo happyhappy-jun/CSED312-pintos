470c593c94b4abb5479c150a7c8c3bb950f0df5d
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-11-22 23:21:00 +0900

    load_segment() frame 사용하도록 변경

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -12,6 +12,7 @@
 #include "userprog/gdt.h"
 #include "userprog/pagedir.h"
 #include "userprog/tss.h"
+#include "vm/frame.h"
 #include <debug.h>
 #include <inttypes.h>
 #include <round.h>
@@ -475,20 +476,20 @@ load_segment(struct file *file, off_t ofs, uint8_t *upage,
     size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
     /* Get a page of memory. */
-    uint8_t *kpage = palloc_get_page(PAL_USER);
+    uint8_t *kpage = frame_alloc(upage, PAL_USER);
     if (kpage == NULL)
       return false;
 
     /* Load this page. */
     if (file_read(file, kpage, page_read_bytes) != (int) page_read_bytes) {
-      palloc_free_page(kpage);
+      frame_free(kpage);
       return false;
     }
     memset(kpage + page_read_bytes, 0, page_zero_bytes);
 
     /* Add the page to the process's address space. */
     if (!install_page(upage, kpage, writable)) {
-      palloc_free_page(kpage);
+      frame_free(kpage);
       return false;
     }
 
