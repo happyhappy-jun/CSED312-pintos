69a93da7dd5674de7ba118414ead39445e8cfcf6
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-11-22 22:36:12 +0900

    frame 기본 틀 추가

---
+++ src/vm/frame.h
@@ -0,0 +1,28 @@
+//
+// Created by 김치헌 on 2023/11/22.
+//
+
+#ifndef PINTOS_SRC_VM_FRAME_H_
+#define PINTOS_SRC_VM_FRAME_H_
+
+#include "threads/thread.h"
+#include "threads/palloc.h"
+#include <hash.h>
+
+struct hash frame_table;
+
+struct frame {
+  void *kpage;
+  void *upage;
+  struct thread *thread;
+  struct hash_elem elem;
+};
+
+void frame_table_init(void);
+unsigned frame_table_hash(const struct hash_elem *, void * UNUSED);
+bool frame_table_less(const struct hash_elem *, const struct hash_elem *, void * UNUSED);
+
+void *frame_alloc(void *, enum palloc_flags);
+void frame_free(void *);
+
+#endif//PINTOS_SRC_VM_FRAME_H_
