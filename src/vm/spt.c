ea77123b9052fbe467a63793da13aa6290999989
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-11-23 13:54:21 +0900

    spt baseline 추가

---
+++ src/vm/spt.c
@@ -0,0 +1,200 @@
+//
+// Created by 김치헌 on 2023/11/23.
+//
+
+#include "spt.h"
+#include "string.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
+#include "userprog/pagedir.h"
+#include "vm/frame.h"
+#include "filesys/file.h"
+
+static struct spt_entry *spt_add(struct spt *, struct spt_entry *);
+static void spt_remove_helper(struct hash_elem *, void * UNUSED);
+static struct hash_elem *spt_get_hash_elem(struct spt *, void *);
+static struct spt_entry *spt_make_clean_spt_entry(void *, bool, bool);
+static void spt_load_page_into_frame_from_file(struct spt_entry *);
+static void spt_load_page_into_frame_from_swap(struct spt_entry *);
+
+
+void spt_init(struct spt *spt) {
+  hash_init(&spt->spt, spt_hash, spt_less, NULL);
+}
+
+
+void spt_destroy(struct spt *spt) {
+  // Todo: 7. On process termination
+  // spt_remove_helper() will free each spt_entry in spt.
+  hash_destroy(&spt->spt, spt_remove_helper);
+}
+
+unsigned spt_hash(const struct hash_elem *elem, void *aux UNUSED) {
+  struct spt_entry *spte = hash_entry(elem, struct spt_entry, elem);
+  return hash_bytes(spte->upage, sizeof(spte->upage));
+}
+
+bool spt_less(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED) {
+  struct spt_entry *spte_a = hash_entry(a, struct spt_entry, elem);
+  struct spt_entry *spte_b = hash_entry(b, struct spt_entry, elem);
+  return spte_a->upage < spte_b->upage;
+}
+
+struct spt_entry *spt_get_entry(struct spt *spt, void *upage) {
+  struct hash_elem *e = spt_get_hash_elem(spt, upage);
+  if (e) {
+    return hash_entry(e, struct spt_entry, elem);
+  }
+  return NULL;
+}
+
+static struct hash_elem *spt_get_hash_elem(struct spt *spt, void *upage) {
+  struct spt_entry spte;
+  spte.upage = upage;
+  return hash_find(&spt->spt, &spte.elem);
+}
+
+// return added spt_entry if success, else ...
+static struct spt_entry *spt_add(struct spt* spt, struct spt_entry* spte) {
+  struct hash_elem *e = hash_insert(&spt->spt, &spte->elem);
+  if (e == NULL)
+    return spte;
+  struct spt_entry *existing_spte = hash_entry(e, struct spt_entry, elem);
+  // Todo: what if spt already has the same entry with given upage?
+  // 1. overwrite the given info into existing entry
+  // 2. ignore the given info
+  // 3. PANIC
+
+  // free spte that is not added to spt
+  if (spte->file_info)
+    free(spte->file_info);
+  free(spte);
+  return existing_spte;
+}
+
+static struct spt_entry *spt_make_clean_spt_entry(void *upage, bool writable, bool is_file) {
+  struct spt_entry *spte = malloc(sizeof(struct spt_entry));
+  if (spte == NULL)
+    PANIC("malloc new spt_entry failed");
+  spte->upage = upage;
+  spte->kpage = NULL;
+  spte->is_loaded = false;
+  spte->writable = writable;
+  spte->is_file = is_file;
+  spte->is_swapped = false;
+  // Todo: swap_index initial value (SWAP_ERROR?)
+  spte->swap_index = 0;
+  return spte;
+}
+
+struct spt_entry *spt_add_file(struct spt *spt, void *upage, bool writable, struct file *file, off_t ofs, uint32_t read_bytes, uint32_t zero_bytes) {
+  struct spt_entry* spte = spt_make_clean_spt_entry(upage, writable, true);
+  struct spt_entry_file_info *file_info = malloc(sizeof(struct spt_entry_file_info));
+  file_info->file = file;
+  file_info->ofs = ofs;
+  file_info->read_bytes = read_bytes;
+  file_info->zero_bytes = zero_bytes;
+  spte->file_info = file_info;
+  return spt_add(spt, spte);
+}
+
+struct spt_entry *spt_add_anon(struct spt* spt, void *upage, bool writable) {
+  struct spt_entry *spte = spt_make_clean_spt_entry(upage, writable, false);
+  spte->file_info = NULL;
+  return spt_add(spt, spte);
+}
+
+// Free spt_entry
+// DESTRUCTOR for hash_destroy()
+static void spt_remove_helper(struct hash_elem *elem, void *aux UNUSED) {
+  struct spt_entry *spte = hash_entry(elem, struct spt_entry, elem);
+  if (spte->is_loaded)
+    spt_evict_page_from_frame(spte);
+  if (spte->file_info)
+    free(spte->file_info);
+
+  // Todo: Don't sure if this is correct to free the spte itself in spt_remove_helper().
+  // should read hash.h and hash.c to check DESTRUCTOR.
+  free(spte);
+}
+
+// Remove spt_entry by upage
+void spt_remove_by_upage(struct spt *spt, void *upage) {
+  struct hash_elem *e = spt_get_hash_elem(spt, upage);
+  if (e) {
+    spt_remove_by_entry(spt, hash_entry(e, struct spt_entry, elem);
+  }
+}
+
+// Remove spt_entry
+void spt_remove_by_entry(struct spt *spt, struct spt_entry *spte) {
+  hash_delete(&spt->spt, &spte->elem);
+  spt_remove_helper(&spte->elem, NULL);
+}
+
+// Load page into frame
+// The caller should install the loaded page by install_page()
+void spt_load_page_into_frame(struct spt_entry *spte) {
+  ASSERT(!spte->is_loaded);
+  ASSERT(spte->kpage == NULL);
+  if (spte->is_file) {
+    spt_load_page_into_frame_from_file(spte);
+  } else {
+    spt_load_page_into_frame_from_swap(spte);
+  }
+}
+
+static void spt_load_page_into_frame_from_file(struct spt_entry *spte) {
+  ASSERT(spte->is_file);
+  if (spte->is_swapped) {
+    // writable file page can be swapped out!
+    ASSERT(spte->writable)
+    spt_load_page_into_frame_from_swap(spte);
+  } else {
+    // Get a frame from memory.
+    spte->kpage = frame_alloc(spte->upage, PAL_USER);
+
+    // Load this page.
+    if (file_read(spte->file_info->file, spte->kpage, spte->file_info->read_bytes) != (int) spte->file_info->read_bytes) {
+      PANIC("file_read failed");
+    }
+    memset(spte->kpage + spte->file_info->read_bytes, 0, spte->file_info->zero_bytes);
+  }
+}
+
+static void spt_load_page_into_frame_from_swap(struct spt_entry *spte) {
+  ASSERT(spte->is_swapped);
+  // Todo: swap_index validation check
+
+  // Get a frame from memory.
+  spte->kpage = frame_alloc(spte->upage, PAL_USER);
+
+  // Load this page.
+  // Todo: swap in from swap disk.
+  spte->is_swapped = false;
+  // vm_swap_in(spte->swap_index, spte->kpage); // maybe?
+  // spte->swap_index = SWAP_ERROR;
+
+  spte->is_loaded = true;
+}
+
+void spt_evict_page_from_frame(struct spt_entry *spte) {
+  ASSERT(spte->is_loaded);
+  ASSERT(spte->kpage != NULL);
+
+  bool is_dirty = pagedir_is_dirty(thread_current()->pagedir, spte->upage);
+
+  // 1. dirty file
+  // 2. anon page (whether dirty or not)
+  // will be swapped out when an eviction occurs.
+
+  if (is_dirty || !spte->is_file) {
+    spte->is_swapped = true;
+    // Todo: swap out into swap disk.
+    // spte->swap_index = vm_swap_out(spte->swap_index, spte->kpage); // maybe?
+  }
+
+  frame_free(spte->kpage);
+  spte->is_loaded = false;
+  spte->kpage = NULL;
+}
---
+++ src/vm/spt.h
@@ -0,0 +1,53 @@
+//
+// Created by 김치헌 on 2023/11/23.
+//
+
+#ifndef PINTOS_SRC_VM_SPT_H_
+#define PINTOS_SRC_VM_SPT_H_
+
+#include "filesys/off_t.h"
+#include <hash.h>
+
+struct spt {
+  struct hash spt;
+};
+
+struct spt_entry_file_info {
+  struct file *file; // file pointer
+  off_t ofs;  // offset
+  uint32_t read_bytes; // read bytes
+  uint32_t zero_bytes; // zero bytes
+};
+
+struct spt_entry {
+  /* general info */
+  void *upage; // user page
+  void *kpage; // backed kernel page; NULL if frame not allocated, else frame->kpage.
+  bool is_loaded; // true if frame allocated, else false.
+  bool writable; // true if page is writable, else false.
+  bool is_file; // true if file-backed page(executable/memory map), else (stack, etc) false.
+  struct hash_elem elem; // hash element for spt.
+
+  /* file info */
+  struct spt_entry_file_info *file_info; // file info; NULL if not file-backed, else file_info.
+
+  /* anon info */
+  bool is_swapped; // true if swapped, else false.
+  // Todo: change to swap_index_t, after implementing swap table.
+  unsigned swap_index; // swap index; 0 if not in swap, else swap->index.
+};
+
+void spt_init(struct spt *);
+void spt_destroy(struct spt *);
+unsigned spt_hash(const struct hash_elem *, void * UNUSED);
+bool spt_less(const struct hash_elem *, const struct hash_elem *, void * UNUSED);
+
+struct spt_entry *spt_get_entry(struct spt *, void *);
+struct spt_entry *spt_add_file(struct spt *, void *, bool, struct file *, off_t, uint32_t, uint32_t);
+struct spt_entry *spt_add_anon(struct spt *, void *, bool);
+void spt_remove_by_upage(struct spt *, void *);
+void spt_remove_by_entry(struct spt *, struct spt_entry *);
+void spt_load_page_into_frame(struct spt_entry *);
+void spt_evict_page_from_frame(struct spt_entry *);
+
+#endif//PINTOS_SRC_VM_SPT_H_
