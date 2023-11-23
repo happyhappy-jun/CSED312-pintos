d31d9e0a34d6aded5e1d45b40e215a711f725981
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-11-23 20:06:54 +0900

    process.c install_page() public으로 변경

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -538,8 +538,7 @@ setup_stack(void **esp) {
    with palloc_get_page().
    Returns true on success, false if UPAGE is already mapped or
    if memory allocation fails. */
-static bool
-install_page(void *upage, void *kpage, bool writable) {
+bool install_page(void *upage, void *kpage, bool writable) {
   struct thread *t = thread_current();
 
   /* Verify that there's not already a page at that virtual
