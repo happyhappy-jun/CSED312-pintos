787417d8cab9b66a38984163b943be53c9231982
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-12-01 05:17:16 +0900

    debug: turn off priority donation

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -198,7 +198,7 @@ void lock_acquire(struct lock *lock) {
 
   struct thread *current_thread = thread_current();
 
-  if (thread_mlfqs) {
+  if (!thread_mlfqs) {
     /* Original lock_acquire() */
     sema_down(&lock->semaphore);
     lock->holder = thread_current();
@@ -244,7 +244,7 @@ void lock_release(struct lock *lock) {
   ASSERT(lock != NULL);
   ASSERT(lock_held_by_current_thread(lock));
 
-  if (thread_mlfqs) {
+  if (!thread_mlfqs) {
     /* Original lock_release */
     lock->holder = NULL;
     sema_up(&lock->semaphore);
