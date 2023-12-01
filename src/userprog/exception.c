4ad31199dcb03abeeaea7997a99508a66926636e
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-12-01 16:32:46 +0900

    remove page_fault() tail

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -174,21 +174,10 @@ page_fault(struct intr_frame *f) {
     f->eax = 0xffffffff;
     return;
   }
-  // other userspace page fault => exit(-1)
-  else if (user) {
-    thread_current()->pcb->exit_code = -1;
-    thread_exit();
-  }
 
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
-  printf("Page fault at %p: %s error %s page in %s context.\n",
-         fault_addr,
-         not_present ? "not present" : "rights violation",
-         write ? "writing" : "reading",
-         user ? "user" : "kernel");
-  kill(f);
+
+  thread_current()->pcb->exit_code = -1;
+  thread_exit();
 }
 
 static bool stack_growth(void *fault_addr, void *esp) {
