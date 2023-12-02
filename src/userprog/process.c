6cba07ec8860f391f3a56a99db41687a3a8d6f04
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-12-03 02:42:45 +0900

    debug: print exit start

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -138,7 +138,7 @@ int process_wait(tid_t child_tid) {
 void process_exit(void) {
   struct thread *cur = thread_current();
   uint32_t *pd;
-
+  printf("[tid:%d] process_exit()\n", cur->tid);
 #ifdef VM
   mmap_destroy(&cur->mmap_list);
   spt_destroy(&cur->spt);
