f4a32c6b956c03bec08c9c0c6ab7dadaf6da5c53
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-12-03 01:28:22 +0900

    Revert "remove kernel buffer in load/unload_page_data()"
    This reverts commit 9fd3d7db

--- src/vm/page.c
+++ src/vm/page.c
@@ -15,10 +15,10 @@
 #include "vm/frame.h"
 
 extern struct lock file_lock;
-static void load_file(void *kpage, struct spt_entry *spte);
-static void load_swap(void *kpage, struct spt_entry *spte);
-static void unload_file(void *kpage, struct spt_entry *spte);
-static void unload_swap(void *kpage, struct spt_entry *spte);
+static void load_file(void *kbuffer, struct spt_entry *spte);
+static void load_swap(void *kbuffer, struct spt_entry *spte);
+static void unload_file(void *kbuffer, struct spt_entry *spte);
+static void unload_swap(void *kbuffer, struct spt_entry *spte);
 
 bool load_page(struct spt *spt, struct spt_entry *spte) {
   void *kpage = frame_alloc(spte->upage, PAL_USER);
@@ -43,24 +43,28 @@ bool unload_page(struct spt *spt, struct spt_entry *spte) {
 
 
 bool load_page_data(void *kpage, struct spt *spt, struct spt_entry *spte) {
+  void *kbuffer = palloc_get_page(PAL_ZERO);
   switch (spte->location) {
   case LOADED:
     printf("[tid:%d] waiting %p:%p (%d, %p)\n", thread_current()->tid, spte->upage, spte->kpage, frame_find(spte->kpage)->thread->tid, frame_find(spte->kpage)->upage);
+    palloc_free_page(kbuffer);
     while(spte->location == LOADED);
     printf("[tid:%d] waiting done\n", thread_current()->tid);
     return load_page_data(kpage, spt, spte);
   case FILE:
-    load_file(kpage, spte);
+    load_file(kbuffer, spte);
     break;
   case SWAP:
-    load_swap(kpage, spte);
+    load_swap(kbuffer, spte);
     break;
   case ZERO:
-    memset(kpage, 0, PGSIZE);
+    memset(kbuffer, 0, PGSIZE);
     break;
   default:
     return false;
   }
+  memcpy(kpage, kbuffer, PGSIZE);
+  palloc_free_page(kbuffer);
   spte->location = LOADED;
   spte->kpage = kpage;
   return install_page(spte->upage, spte->kpage, spte->writable);
@@ -76,17 +80,23 @@ bool unload_page_data(struct spt *spt, struct spt_entry *spte) {
   }
   pagedir_clear_page(spt->pagedir, spte->upage);
 
+  void *kbuffer = NULL;
+  if (dirty) {
+    kbuffer = palloc_get_page(PAL_ZERO);
+    memcpy(kbuffer, spte->kpage, PGSIZE);
+  }
+
   switch (spte->type) {
   case MMAP:
     if (dirty) {
-      unload_file(spte->kpage, spte);
+      unload_file(kbuffer, spte);
       spte->dirty = false;
     }
     spte->location = FILE;
     break;
   case EXEC:
     if (dirty) {
-      unload_swap(spte->kpage, spte);
+      unload_swap(kbuffer, spte);
       spte->location = SWAP;
     } else {
       spte->location = FILE;
@@ -94,7 +104,7 @@ bool unload_page_data(struct spt *spt, struct spt_entry *spte) {
     break;
   case STACK:
     if (dirty) {
-      unload_swap(spte->kpage, spte);
+      unload_swap(kbuffer, spte);
       spte->location = SWAP;
     } else {
       spte->location = ZERO;
@@ -103,52 +113,54 @@ bool unload_page_data(struct spt *spt, struct spt_entry *spte) {
   default:
     return false;
   }
+  if (dirty)
+    palloc_free_page(kbuffer);
   ASSERT(spte->location != LOADED)
   return true;
 }
 
-static void load_file(void *kpage, struct spt_entry *spte) {
-  ASSERT(kpage != NULL)
+static void load_file(void *kbuffer, struct spt_entry *spte) {
+  ASSERT(kbuffer != NULL)
   ASSERT(spte->location == FILE)
   ASSERT(spte->file_info != NULL)
   struct file_info *file_info = spte->file_info;
 
   lock_acquire(&file_lock);
-  int read_bytes = file_read_at(file_info->file, kpage, (int) file_info->read_bytes, file_info->offset);
+  int read_bytes = file_read_at(file_info->file, kbuffer, (int) file_info->read_bytes, file_info->offset);
   lock_release(&file_lock);
-  memset(kpage + read_bytes, 0, (int) file_info->zero_bytes);
+  memset(kbuffer + read_bytes, 0, (int) file_info->zero_bytes);
 
   if (read_bytes != (int) file_info->read_bytes) {
     PANIC("Failed to read file");
   }
 }
 
-static void load_swap(void *kpage, struct spt_entry *spte) {
-  ASSERT(kpage != NULL)
+static void load_swap(void *kbuffer, struct spt_entry *spte) {
+  ASSERT(kbuffer != NULL)
   ASSERT(spte->location == SWAP)
   ASSERT(spte->swap_index != -1)
 
-  swap_in(spte->swap_index, kpage);
+  swap_in(spte->swap_index, kbuffer);
   spte->swap_index = -1;
 }
 
-static void unload_file(void *kpage, struct spt_entry *spte) {
-  ASSERT(kpage != NULL)
+static void unload_file(void *kbuffer, struct spt_entry *spte) {
+  ASSERT(kbuffer != NULL)
   ASSERT(spte->file_info != NULL)
 
   struct file_info *file_info = spte->file_info;
 
   lock_acquire(&file_lock);
-  int write_bytes = file_write_at(file_info->file, kpage, (int) file_info->read_bytes, file_info->offset);
+  int write_bytes = file_write_at(file_info->file, kbuffer, (int) file_info->read_bytes, file_info->offset);
   lock_release(&file_lock);
   if (write_bytes != (int) file_info->read_bytes) {
     PANIC("Failed to write file");
   }
 }
 
-static void unload_swap(void *kpage, struct spt_entry *spte) {
-  ASSERT(kpage != NULL)
+static void unload_swap(void *kbuffer, struct spt_entry *spte) {
+  ASSERT(kbuffer != NULL)
   ASSERT(spte->swap_index == -1)
 
-  spte->swap_index = (int) swap_out(kpage);
+  spte->swap_index = (int) swap_out(kbuffer);
 }
