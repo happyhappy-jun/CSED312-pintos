8f983947c9f9c521dde1449f148e44165e0a09d0
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-12-02 19:02:51 +0900

    Resolving merge issue

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -10,6 +10,4 @@ void process_activate(void);
 static char *parse_command(char *);
 bool install_page(void *upage, void *kpage, bool writable);
 
-bool install_page(void *upage, void *kpage, bool writable);
-
 #endif /* userprog/process.h */
