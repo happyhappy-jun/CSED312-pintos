77ea326f41b178184de0f78be79a3f6351f1d567
Kim Chiheon <rlaclgjs1107@gmail.com>
2023-12-03 02:56:11 +0900

    debug: add prints at spte_destroy() start/end

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -38,6 +38,7 @@ void spt_destroy(struct spt *spt) {
 
 static void spte_destroy(struct hash_elem *elem, void *aux) {
   struct spt_entry *spte = hash_entry(elem, struct spt_entry, elem);
+  printf("[tid:%d] spte_destroy: %p:%p\n", thread_current()->tid, spte->upage, spte->kpage);
   if (spte->location == LOADED) {
     if (!frame_test_and_pin(spte->kpage)) {
       printf("[tid:%d] waiting 2\n", thread_current()->tid);
@@ -46,7 +47,6 @@ static void spte_destroy(struct hash_elem *elem, void *aux) {
       }
       printf("[tid:%d] waiting 2 end\n", thread_current()->tid);
     } else {
-      printf("[tid:%d] unload LOADED spte: %p:%p\n", thread_current()->tid, spte->upage, spte->kpage);
       unload_page(&thread_current()->spt, spte);
     }
   }
@@ -56,6 +56,7 @@ static void spte_destroy(struct hash_elem *elem, void *aux) {
   if (spte->type == EXEC || spte->type == MMAP) {
     free(spte->file_info);
   }
+  printf("[tid:%d] spte_destroy: %p:%p end\n", thread_current()->tid, spte->upage, spte->kpage);
   free(spte);
 }
 
